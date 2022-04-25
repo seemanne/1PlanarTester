bd52d05ec97bf08b587fccfe8c897c662231c29c
Emanuel Seemann <3380606+emionline@users.noreply.github.com>
2022-04-25 14:19:43 +0200

    added computeInducedGraph function

--- 1PlanarTester.py
+++ 1PlanarTester.py
@@ -15,7 +15,7 @@ def generateE(G):
     #no difference atm
     for i in range(n):
         for j in range(n-i-1):
-            l2.append((l1[i], l1[j+1]))
+            l2.append((l1[i], l1[i+j+1]))
     E = np.array(l2)
     return E
 
@@ -53,6 +53,7 @@ def checkLegalCrossings(y, E):
             else: return False
     return True
 
+#returns a set containing all edges that are crossed, without caring about the edges they are crossed with
 def findCrossedEdges(y, E):
     S = {}
     n = len(y)
@@ -61,6 +62,7 @@ def findCrossedEdges(y, E):
             S.union({E[i][0], E[i][1]})
     return S
     
+#returs a set containing all edges that are element of a kite in the drawing (even if the full kite doesn't exist)
 def findKiteEdges(y, E, am):
     n = len(y)
     S = {}
@@ -74,7 +76,36 @@ def findKiteEdges(y, E, am):
             if(am[v2][w2] == 1): S.add([v2, w2])
     return S
 
-def computeInducedGraph():
+def computeInducedGraph(y, G , E, am, cross_edges, kite_edges):
+    edgeList = []
+    #edges of type a)
+    n = len(cross_edges)
+    for i in range(n):
+        edgeList.append(cross_edges[i])
+    #edges of type b)
+    index = len(y)
+    #E[i] has shape [[a b][x y]]
+    a = E[index][0][0]
+    b = E[index][0][1]
+    for i in range(a):
+        #we use b-2 here because we want to add edges up to [a b-1] but need to care not to add edges like [0 0]
+        for j in range(b-2):
+            edgeList.append([a, b+1])
+    #add [a, b] to the edge list if the next crossing in E starts with a different edge
+    if (E[index+1][0] != np.array([(a, b)])):
+        edgeList.append([a, b])
+    #we do not implement c because its too expensive to check with too little gain (we need to check almost n^2 edges for every edge and its rarely true)
+    #edges of type d)
+    n = len(kite_edges)
+    for i in range(n):
+        edgeList.add(kite_edges[i])
+    #build the graph Gv
+    Gv = nx.Graph()
+    Gv.add_edges_from(edgeList)
+    return Gv
+
+
+
     
 
 def verifyNode(y, G, E):
