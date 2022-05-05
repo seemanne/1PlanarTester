c4703a26113889962ab8de5f9c1ddf3a77862565
Emanuel Seemann <3380606+emionline@users.noreply.github.com>
2022-05-05 23:07:16 +0200

    removed unnecessary debug statements

--- test.py
+++ test.py
@@ -13,23 +13,7 @@ adjacency_dict = {
 7: (0, 3, 4, 5, 6)}
 sys.stdout = open("log.txt", "w")
 G = nx.Graph(adjacency_dict)
-#G = nx.complete_graph(6)
 E = opt.generateE(G)
-n = len(E)
-yi = []
-for i in range(n):
-    j = 0
-    if(i == 36):
-        j = 1
-    if(i == 49):
-        j = 1
-    if(i == 97):
-        j = 1
-    if(i == 126):
-        j = 1
-    yi.append(j)
-    #print(i, " ", E[i])
-#opt.createLog(1, yi, E, len(G))
 x, y = opt.searchTree([0], G, E)
 print(x, y)
 opt.createLog(x, y, E, len(G))
