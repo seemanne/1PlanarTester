7622b7aa5defd32f120f1020ae33ecb396d86b67
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2022-05-05 13:53:38 +0200

    fixed edge insertion

--- MaxOnePlanarTester.py
+++ MaxOnePlanarTester.py
@@ -6,13 +6,25 @@ import networkx as nx
 def maximalityTester(G):
     l = list(nx.non_edges(G))
     isMaximal = True
-    for i in len(l):
+    for i in range(len(l)):
         Gtemp = nx.Graph()
         Gtemp.add_edges_from(G.edges)
-        Gtemp.add_edge(l[i])
+        Gtemp.add_edge(l[i][0],l[i][1])
         E = opt.generateE(Gtemp)
         x, y = opt.searchTree([],Gtemp,E, verbose = False)
         if(x == 0):
             isMaximal = False
     return isMaximal
 
+adjacency_dict = {
+0: (1, 2, 3, 4, 5, 6, 7),
+1: (0, 2, 3, 4, 5, 6),
+2: (0, 1, 3),
+3: (0, 1, 2, 4, 5, 7),
+4: (0, 1, 3, 5, 6, 7),
+5: (0, 1, 3, 4, 6, 7),
+6: (0, 1, 4, 5, 7),
+7: (0, 3, 4, 5, 6)}
+G = nx.Graph(adjacency_dict)
+maximal = maximalityTester(G)
+print(maximal)
